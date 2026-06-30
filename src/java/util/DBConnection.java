56db62f179e9f36dc949ad2c8ce0274f66c7bf3d
dee204 <farajuhaimi2411@gmail.com>
2026-07-01 03:37:51 +0800

    Use Kerocket database URL

--- src/java/util/DBConnection.java
+++ src/java/util/DBConnection.java
@@ -10,18 +10,55 @@ import java.sql.DriverManager;
  * @author farajuhaimi
  */
 public class DBConnection {
-   private static final String URL = "jdbc:mysql://localhost:3307/fyp";
-    private static final String USER = "root";
-    private static final String PASS = "";
+   private static final String LOCAL_URL = "jdbc:mysql://localhost:3307/fyp";
+private static final String LOCAL_USER = "root";
+private static final String LOCAL_PASS = "";
 
-    public static Connection getConnection() {
-        Connection con = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(URL, USER, PASS);
-        } catch (Exception e) {
-            e.printStackTrace();
+public static Connection getConnection() {
+    Connection con = null;
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        if (databaseUrl != null && !databaseUrl.isEmpty()) {
+            con = DriverManager.getConnection(convertToJdbcUrl(databaseUrl));
+        } else {
+            con = DriverManager.getConnection(LOCAL_URL, LOCAL_USER, LOCAL_PASS);
         }
-        return con;
+
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+
+    return con;
+}
+
+private static String convertToJdbcUrl(String databaseUrl) throws Exception {
+    if (databaseUrl.startsWith("jdbc:mysql://")) {
+        return databaseUrl;
+    }
+
+    java.net.URI uri = new java.net.URI(databaseUrl);
+
+    String userInfo = uri.getUserInfo();
+    String user = "";
+    String password = "";
+
+    if (userInfo != null && userInfo.contains(":")) {
+        String[] parts = userInfo.split(":", 2);
+        user = parts[0];
+        password = parts[1];
+    }
+
+    String host = uri.getHost();
+    int port = uri.getPort();
+    String database = uri.getPath();
+
+    return "jdbc:mysql://" + host + ":" + port + database
+            + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC"
+            + "&user=" + user
+            + "&password=" + password;
+}
 }
